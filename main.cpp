5b21e6b89a368be51329ea54335fc7c283700172
Leon <l-dzondervan@hotmail.com>
2019-04-14 18:06:49 +0200

    Add dependency glm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
@@ -15,8 +19,9 @@ void initWindow() {
 }
 
 int main() {
+    glm::vec2 vector = glm::vec2(0.5);
     initWindow();
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << vector.y << std::endl;
     while(true) {}
     return 0;
 }
