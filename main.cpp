7bbe5a5b8e9ae24a217518d6e45dc422b61e6e02
Leon <l-dzondervan@hotmail.com>
2019-04-14 16:39:08 +0200

    cmake dependency testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+}
+
 int main() {
+    initWindow();
     std::cout << "Hello, World!" << std::endl;
+    while(true) {}
     return 0;
 }
