a5eaa24802c0adc359368546a753fad2a611ee09
Leon <l-dzondervan@hotmail.com>
2019-04-30 23:06:51 +0200

    Basic material system and refactoring

---
+++ src/Models/Vertex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Leon on 4/30/2019.
+//
+
+#include "Vertex.h"
---
+++ src/Models/Vertex.h
@@ -0,0 +1,14 @@
+//
+// Created by Leon on 4/30/2019.
+//
+
+#ifndef ALPACAWEB_VERTEX_H
+#define ALPACAWEB_VERTEX_H
+
+
+class Vertex {
+
+};
+
+
+#endif //ALPACAWEB_VERTEX_H
