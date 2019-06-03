af2ea2e1d4fc967a9811391f930f56ff9618ced0
ObeA <obe@obeattema.nl>
2019-06-03 13:12:35 +0200

    Remove build folder

--- build/glm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/obe/dev/projects/school/graphics/alpacanet-the-game/glm
-# Build directory: /home/obe/dev/projects/school/graphics/alpacanet-the-game/build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
--- build/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/obe/dev/projects/school/graphics/alpacanet-the-game/glm/glm
-# Build directory: /home/obe/dev/projects/school/graphics/alpacanet-the-game/build/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
